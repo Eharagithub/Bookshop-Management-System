8ac0a36c98c271149cb62dde10865ca007cfaf78
Eharagithub <39-bit-0016@kdu.ac.lk>
2024-04-29 00:33:25 +0530

    award book

--- client/src/home/FavBook.jsx
+++ client/src/home/FavBook.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FavBookImg from "../assets/favoritebook.jpg"
+import { Link } from 'react-router-dom'
 
 const FavBook = () => {
   return (
@@ -11,6 +12,7 @@ const FavBook = () => {
         <div className='md:w1/2 space-y-6'>
             <h2 className='text-5xl font-bold my-5 md:w-3/4 leading-snug'>Find Your Favourite <span className='text-blue-700'>Book Here!</span></h2>
             <p className='mb-10 text-lg md:w-5/6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem explicabo placeat, velit hic rem, quaerat aperiam provident cupiditate enim doloribus quibusdam consectetur! Ullam quidem unde aut porro debitis harum labore?</p>
+           
             <div className='flex flex-col sm:flex-row justify-between gap-6 md:w-3/4 my-14'>
                 <div>
                     <h3 className='text-3xl font-bold'>800+</h3>
@@ -25,6 +27,8 @@ const FavBook = () => {
                     <p className='text-base'>PDF Downloads</p>
                 </div>
             </div>
+
+            <Link to="/Shop" className='mt-12 block'><button className='bg-blue-700 text-white font-semibold px-5 py-2 rounded hover:bg-black transition-all duration-300'>Explore More</button></Link>
         </div>
     </div>
   )
