9f209ececbe41ecca69d64b20b4e23e0cdf86724
Eharagithub <39-bit-0016@kdu.ac.lk>
2024-04-30 10:10:09 +0530

    manage book interface

--- client/src/dashboard/ManageBook.jsx
+++ client/src/dashboard/ManageBook.jsx
@@ -1,8 +1,89 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Table } from "flowbite-react";
 
 const ManageBook = () => {
+  const [allBooks, setAllBooks] = useState([]);
+  useEffect(() =>{
+    fetch(" ").then(res => res.json()).then(data => setAllBooks(data))
+  },[])
+
   return (
-    <div>ManageBook</div>
+    <div className='px-4 my-12'>
+      <h2 className='mb-8 text-3xl font-bold'>Manage Your Book</h2>
+
+      {/*table for book data */}
+      <Table striped className='lg:w-[1280px]'>
+
+        <Table.Head>
+          <Table.HeadCell>No:</Table.HeadCell>
+          <Table.HeadCell>Book Name</Table.HeadCell>
+          <Table.HeadCell>Author Name</Table.HeadCell>
+          <Table.HeadCell>Category</Table.HeadCell>
+          <Table.HeadCell>Price</Table.HeadCell>
+          <Table.HeadCell>
+            <span>Edit or Manage</span>
+          </Table.HeadCell>
+        </Table.Head>
+        
+        <Table.Body className="divide-y">
+
+          {/*table row 1 */}
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+          <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {'01'}
+            </Table.Cell>
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {'Apple MacBook Pro 17"'}
+            </Table.Cell>
+            <Table.Cell>Sliver</Table.Cell>
+            <Table.Cell>Laptop</Table.Cell>
+            <Table.Cell>$2999</Table.Cell>
+            <Table.Cell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500 mr-5">
+                Edit
+              </a>
+              <button onClick={() => handDelete(book._id)} className='bg-red-600 px-5 py-1 font-semibold text-white rounded-sm hover:bg-sky-600'>Delete</button>
+            </Table.Cell>
+          </Table.Row>
+
+          {/*table row 2 */}
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+          <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {'02'}
+            </Table.Cell>
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              Microsoft Surface Pro
+            </Table.Cell>
+            <Table.Cell>White</Table.Cell>
+            <Table.Cell>Laptop PC</Table.Cell>
+            <Table.Cell>$1999</Table.Cell>
+            <Table.Cell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </Table.Cell>
+          </Table.Row>
+
+          {/*table row 3 */}
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+          <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {'03'}
+            </Table.Cell>
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</Table.Cell>
+            <Table.Cell>Black</Table.Cell>
+            <Table.Cell>Accessories</Table.Cell>
+            <Table.Cell>$99</Table.Cell>
+            <Table.Cell>
+              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                Edit
+              </a>
+            </Table.Cell>
+          </Table.Row>
+          
+
+        </Table.Body>
+      </Table>
+    </div>
   )
 }
 
