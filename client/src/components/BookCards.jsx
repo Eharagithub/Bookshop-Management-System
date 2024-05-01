4b37b5f04927ec62d35d13b7b89db21757275204
GDT Thanuja <135541581+Thanu0109@users.noreply.github.com>
2024-05-01 11:04:34 -0700

    Book Cards

--- client/src/components/BookCards.jsx
+++ client/src/components/BookCards.jsx
@@ -11,8 +11,12 @@ import 'swiper/css/pagination';
 
 // import required modules
 import { FreeMode, Pagination } from 'swiper/modules';
+import { Link } from 'react-router-dom'; // Adjust the import path based on your routing library
+import { FaCartShopping } from 'react-icons/fa6';
+
 
 const BookCards = ({headline,books}) => {
+  console.log(books);
 
   return (
     <div className='my-16 px-4 lg:px-24'>
@@ -27,18 +31,43 @@ const BookCards = ({headline,books}) => {
         pagination={{
           clickable: true,
         }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
         modules={[FreeMode, Pagination]}
         className="mySwiper w-full h-full"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+      >{
+        books.map(book =><SwiperSlide key={book._id}>
+          <Link to={`/book/${book._id}`}>
+
+            <div className='relative'>
+              <img src={book.imageURL} alt=""/>
+              <div className=' absolute top-1 right-12 bg-blue-500 hover:bg-black p-2 rounded'>
+                <FaCartShopping className='w-4 h-4 text-white'/>
+              </div>
+            </div>
+            <div>
+              <div>
+              <h3>{book.bookTitle}</h3>
+              <p>{book.authorName}</p>
+              </div>
+          
+
+            </div>
+          </Link>
+        </SwiperSlide>)
+      }
       </Swiper>
         </div>
     </div>
